7ea93e83f78de6e5a178941d0f3a5334c5451666
void-anurag <anuragtamhankar.0@gmail.com>
2023-12-18 17:18:39 +0530

    Added random color for bg and added 10% opacity increase function

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const sizeOfGrid = 16;
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('button');
 
+const createRandomRGB = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return { r, g, b }
+}
+
 const createGrid = (amtOfGrids) => {
     const wrapper = document.createElement('div');
     wrapper.classList.add('wrapper');
@@ -11,15 +19,30 @@ const createGrid = (amtOfGrids) => {
         row.classList.add('grid-row');
 
         for (let j=0; j<amtOfGrids; j++){
+            const { r, g, b } = createRandomRGB();
+
             const widthAndHeight = 960/amtOfGrids;
             const gridBox = document.createElement('div');
-            gridBox.classList.add('gird-box');
+            gridBox.classList.add('grid-box');
             gridBox.style.width = `${widthAndHeight}px`;
             gridBox.style.height = `${widthAndHeight}px`;
             //adding mouseenter listener to change bg
             gridBox.addEventListener('mouseenter', () => {
+                const currentOpacity = Number(gridBox.style.opacity);
+                gridBox.style.background = `rgb(0, 0, 0)`;
+                    gridBox.style.opacity = Number(currentOpacity) + .10;
+            })
+            /*
+            gridBox.addEventListener('mouseenter', () => {
                 gridBox.style.backgroundColor = 'black';
             })
+            */
+            /*
+            gridBox.addEventListener('mouseenter', () => {
+                const bgColor = "rgb(" + r + "," + g + "," + b +  ")";
+                gridBox.style.background = bgColor;
+            })
+            */
             row.appendChild(gridBox);
         }
         
